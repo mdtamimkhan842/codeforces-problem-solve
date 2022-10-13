4e6d6e44f807d9178d95a50be709ca21fb2b48ce
mdtamimkhan842 <mdtamimkhan842@gmail.com>
2022-10-13 23:03:00 +0600

    first commit

---
+++ 1 to n number in c.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void printNumber(int N);
+
+int main(){
+    int N;
+    scanf("%d", &N);
+
+    printNumber(N);
+
+    return 0;
+}
+
+void printNumber(int N){
+    if(N >= 1){
+        printNumber(N-1);
+        printf("%d ", N);
+    }
+}
